2b05d7faf9bcf087d390d66cadafa7b098cb4699
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2022-01-15 12:32:08 -0800

    Update depth_sensor.py

--- src/depth_sensor.py
+++ src/depth_sensor.py
@@ -6,6 +6,10 @@ import time
 from std_msgs.msg import Float32
 
 if __name__ == "__main__":
+    rospy.init_node("depth_sensor")
+    pub = rospy.Publisher('depth_sensor', Float32, queue_size=10)
+    rate = rospy.Rate(10)
+    
     sensor = ms5837.MS5837()
     try:
       sensor.init()
@@ -13,10 +17,6 @@ if __name__ == "__main__":
         rospy.logerr("depth_sensor.py: depth sensor not plugged in")
     else:
         sensor.setFluidDensity(ms5837.DENSITY_FRESHWATER)
-
-        rospy.init_node("depth_sensor")
-        pub = rospy.Publisher('depth_sensor', Float32, queue_size=10)
-        rate = rospy.Rate(10)
 
         while not rospy.is_shutdown():
             try:
