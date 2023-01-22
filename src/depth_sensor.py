2e613a4bc75e2aa851d29c3b59ed6fde93564e2c
James-Randall-14 <jammerand14@gmail.com>
2023-01-22 12:29:33 -0800

    changed from logerr to loginfo

--- src/depth_sensor.py
+++ src/depth_sensor.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     try:
       sensor.init()
     except IOError:
-        rospy.logerr("Cannot connect to depth sensor. Ignore this if the sensor is unpluged.")
+        rospy.loginfo("Cannot connect to depth sensor. Ignore this if the sensor is unplugged.")
         connected = False
         while not connected:
             try:
