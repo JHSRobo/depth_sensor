055d2849dce70c47e898b3fa43bff8891b7ae00b
James-Randall-14 <jammerand14@gmail.com>
2023-05-29 10:59:10 -0700

    cursory leak detection

--- src/depth_sensor.py
+++ src/depth_sensor.py
@@ -2,11 +2,18 @@
 
 import ms5837
 import rospy
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, Bool
 
 if __name__ == "__main__":
     rospy.init_node("depth_sensor")
+
+    # Set up leak detection pin
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(27, GPIO.IN)
+
     pub = rospy.Publisher('rov/depth_sensor', Float32, queue_size=10)
+    leak_pub = rospy.Publisher('leak_detect', Bool, queue_size=10)
     rate = rospy.Rate(10)
     
     sensor = ms5837.MS5837()
@@ -27,10 +34,13 @@ if __name__ == "__main__":
   
     sensor.setFluidDensity(ms5837.DENSITY_FRESHWATER)
 
+
     while not rospy.is_shutdown():
         try:
             sensor.read()
             pub.publish(sensor.depth())
         except IOError:
             pass
+        if GPIO.input(27):
+            leak_pub.publish(GPIO.input(27))
         rate.sleep()
